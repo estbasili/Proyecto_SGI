7e1ff73d6219e88fe67a1a0200fb2782280db464
Esteban Javier Basili <est.basili@gmail.com>
2024-10-27 14:44:54 -0300

    reformando view login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, make_response,flash
 from flask_login import LoginManager, login_required
+
 from routes.auth import auth_bp
 from models.logUsuario import User
 
@@ -11,17 +12,34 @@ app.register_blueprint(auth_bp)                           # se registran las rut
 # Inicializa Flask-Login inicializacion del sistema de autentificacion
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'auth.login' 
 
 # recupero el usuario que inicio sesion
 @login_manager.user_loader
 def load_user(user_id):
      return User.get_by_id(user_id)
 
+# Redirige siempre al login cuando la aplicación comienza
 @app.route('/')
-@login_required                                           # Asegúrate de que el usuario esté autenticado para acceder a la pagina
+def home():
+     return redirect (url_for('auth.login'))  
+          
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('Por favor, inicia sesión para acceder a esta página.', 'secondary')  # Personaliza el mensaje
+    return redirect(url_for('auth.login'))  # Redirigir al login
+
+# Asegúrate de que el usuario esté autenticado para acceder a la pagina de la app
+@app.route('/home')
+@login_required                                         
 def index():
-    return render_template('index.html')
-    
+  response = make_response(render_template('index.html'))
+  # Desactiva la caché para que el navegador siempre solicite una nueva versión
+  response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+  response.headers['Pragma'] = 'no-cache'
+  response.headers['Expires'] = '-1'
+  return response
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
--- routes/auth.py
+++ routes/auth.py
@@ -1,15 +1,29 @@
 # routes/auth.py
-from flask import Blueprint, request, render_template, redirect, url_for, flash
+from flask import Blueprint, request, render_template, redirect, url_for, flash,make_response
 from db.db_config import get_db_connection
 from models.logUsuario import User 
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 from models.usuario import Usuario
 import bcrypt
 
 auth_bp = Blueprint('auth', __name__)
 
+
+#Protejo a todas las rutas para que solamente se acceda si el usuario esta logeado
+@auth_bp.before_request
+def require_login():
+     if not current_user.is_authenticated and request.endpoint != 'auth.login':
+        return redirect(url_for('auth.login'))  
+
+
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
+    
+    if current_user.is_authenticated:
+        logout_user()  # Cierra la sesión si el usuario ya está autenticado
+        flash('Sesión cerrada automáticamente. Por favor, inicie sesión nuevamente.', 'info')
+        return redirect(url_for('auth.login'))  # Redirige a login después de cerrar la sesión
+    
     if request.method == 'POST':
         nombre = request.form['nombre']
         password = request.form['password']
@@ -24,7 +38,12 @@ def login():
             return redirect(url_for('index'))
         else:
             flash('Nombre de usuario o contraseña incorrectos','danger')
-    return render_template('login.html')
+    response = make_response(render_template('login.html'))
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+    
 
 
 @auth_bp.route('/logout')
@@ -32,6 +51,12 @@ def logout():
     logout_user()  # Cerrar sesión
     flash('Has cerrado sesión exitosamente', 'success')
     return redirect(url_for('auth.login'))  # Redirigir al login
+
+@auth_bp.route('/prueba')
+def prueba():
+    return render_template('prueba.html')
+    
+    
     
 
 
