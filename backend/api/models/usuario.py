5544cad5bcc55c6333a1e1b96389b41b84a296aa
Dana Fernandez <danamfer@gmail.com>
2024-11-17 00:20:01 -0300

    Validaciones usuarios

--- backend/api/models/usuario.py
+++ backend/api/models/usuario.py
@@ -1,4 +1,3 @@
-# models/usuario.py
 from db.db import get_db_connection, DBError
 
 class Usuario:
@@ -9,8 +8,6 @@ class Usuario:
         "id_rol": int,
     }
 
-
-
     @classmethod
     def validar_datos(cls, data):
         if data is None or type(data) != dict:
@@ -19,28 +16,30 @@ class Usuario:
             if key not in data:
             # Si 'id_rol' no está presente en 'data', inicialízalo con 1
                 if key == "id_rol":
-                    data[key] = 1
+                   data[key] = 1
                 else:
                     return False
+            elif key == "id_rol" and (data[key] == "" or type(data[key]) != key_type):  # Acepta "" y verifica tipo para id_rol
+            # Si es una cadena vacía, asigna el valor por defecto
+                data[key] = 1
             elif type(data[key]) != key_type:
                 return False
         return True
+
 
     def __init__(self, data):
         self.id_usuario = data[0]
         self.nombre = data[1]
         self.email = data[2]
         self.contraseña = data[3]
-        # Inicializar id_rol a 1 si no está presente en data
-        self.id_rol = data[4] if len(data) > 4 else 1
-
+        self.id_rol = data[4] if len(data) > 4 else 1  # Asignar id_rol a 1 si no está presente
 
     def a_json(self):
         return {
             "id_usuario": self.id_usuario,
             "nombre": self.nombre,
             "email": self.email,
-            "contarsena": self.contraseña,
+            "contraseña": self.contraseña,
             "id_rol": self.id_rol,
         }
 
@@ -55,62 +54,77 @@ class Usuario:
             usuarios.append({
                 "id_usuario": usuario[0],
                 "nombre": usuario[1],
-                "email":usuario[2],
-                "contrasena":usuario[3],
-                "id_rol":usuario [4],
+                "email": usuario[2],
+                "contraseña": usuario[3],
+                "id_rol": usuario[4],
             })
 
         cursor.close()
         conexion.close()
-        
         return usuarios
 
     @classmethod
-    def get_by_id(cls, id_usuario):
+    def get_by_id(cls, id):
         conexion = get_db_connection()
         cursor = conexion.cursor()
-        cursor.execute("SELECT id_usuario, nombre,email,contraseña,id_rol FROM usuario WHERE id_usuario = %s", (id_usuario,))
+        cursor.execute('SELECT * FROM usuario WHERE id_usuario = %s', (id,))
         data = cursor.fetchone()
         cursor.close()
         conexion.close()
-        
-        return cls(data) if data else None
+        if data:
+            return Usuario(data).a_json()
+        raise DBError('No existe el recurso solicitado')
 
     @classmethod
     def create(cls, data):
         if not cls.validar_datos(data):
-            raise ValueError("Invalid data for Usuario")
+            raise DBError("Campos/valores inválidos")
+        conexion = get_db_connection()
+        cursor = conexion.cursor()
+        # Control si el email no esta en uso por otro usuario
+        email = data["email"]
+        cursor.execute('SELECT * FROM usuario WHERE email = %s', (email,))
+        row = cursor.fetchone()
+        if row is not None:
+            raise DBError("Email ya registrado")
         
-        conexion = get_db_connection()
-        cursor = conexion.cursor()
         cursor.execute(
-            "INSERT INTO usuario (nombre,email,contraseña,id_rol) VALUES (%s,%s,%s,%s)",
-            (data['nombre'],data['email'],data['contraseña'],data['id_rol'])
+            "INSERT INTO usuario (nombre, email, contraseña, id_rol) VALUES (%s, %s, %s, %s)",
+            (data['nombre'], data['email'], data['contraseña'], data['id_rol'])
         )
         conexion.commit()
+        id_usuario = cursor.lastrowid
         cursor.close()
         conexion.close()
+        return {"mensaje": "Usuario creado exitosamente", "id_usuario": id_usuario}
 
     @classmethod
-    def update(cls, id_usuario, data):
+    def update(cls, id, data):
         if not cls.validar_datos(data):
-            raise ValueError("Invalid data for Usuario")
-
+            raise DBError("Campos/valores inválidos")
         conexion = get_db_connection()
         cursor = conexion.cursor()
+        cursor.execute('SELECT * FROM usuario WHERE id_usuario = %s', (id,))
+        if cursor.fetchone() is None:
+            raise DBError("No existe el recurso solicitado")
         cursor.execute(
-            "UPDATE usuario SET nombre = %s,email = %s,contraseña = %s,id_rol = %s, WHERE id_usuario = %s",
-            (data['nombre'], data['email'], data['contraseña'], data['id_rol'], id_usuario)
+            "UPDATE usuario SET nombre = %s, email = %s, contraseña = %s, id_rol = %s WHERE id_usuario = %s",
+            (data['nombre'], data['email'], data['contraseña'], data['id_rol'], id)
         )
         conexion.commit()
         cursor.close()
         conexion.close()
+        return {"mensaje": "Usuario actualizado"}
 
     @classmethod
-    def delete(cls, id_usuario):
+    def delete(cls, id):
         conexion = get_db_connection()
         cursor = conexion.cursor()
-        cursor.execute("DELETE FROM usuario WHERE id_usuario = %s", (id_usuario,)) ### saque una coma que detras de id_categoria
+        cursor.execute('SELECT * FROM usuario WHERE id_usuario = %s', (id,))
+        if cursor.fetchone() is None:
+            raise DBError("No existe el recurso solicitado")
+        cursor.execute('DELETE FROM usuario WHERE id_usuario = %s', (id,))
         conexion.commit()
         cursor.close()
         conexion.close()
+        return {"mensaje": "Usuario eliminado"}
--- backend/api/routes/usuario.py
+++ backend/api/routes/usuario.py
@@ -1,47 +1,62 @@
-# routes/user.py
-
+from db.db import DBError
 from flask import Blueprint, request, jsonify
 from models.usuario import Usuario
 
 usuario_bp = Blueprint('usuario', __name__)
 
-
-#################
-
-# Get all users
+# Obtener todos los usuarios
 @usuario_bp.route('/usuarios', methods=['GET'])
 def get_usuarios():
-    usuarios = Usuario.get_all()
-    return jsonify(usuarios), 200
+    try:
+        usuarios = Usuario.get_all()
+        return jsonify(usuarios), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
 
-# Get user by ID
+# Obtener usuario por ID
 @usuario_bp.route('/usuarios/<int:id_usuario>', methods=['GET'])
 def get_usuario(id_usuario):
-    usuario = Usuario.get_by_id(id_usuario)
-    if usuario:
-        return jsonify(usuario.a_json()), 200
-    return jsonify({"error": "Usuario not found"}), 404
+    try:
+        usuario = Usuario.get_by_id(id_usuario)
+        return jsonify(usuario), 200
+    except DBError as e:
+        return jsonify({"message": str(e)}), 404
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
 
-# Create a new user
+# Crear un nuevo usuario--------------No puedo hacer que ande cuando id_rol es una cadena vacia
 @usuario_bp.route('/usuarios', methods=['POST'])
 def create_usuario():
-    data = request.json
-    if not Usuario.validar_datos(data):
-        return jsonify({"error": "Invalid data"}), 400
-    Usuario.create(data)
-    return jsonify({"message": "User created successfully"}), 201
+    try:
+        data = request.json
+        response = Usuario.create(data)
+        return jsonify(response), 201
+    except DBError as e:
+        return jsonify({"message": str(e)}), 400
+    except Exception as e:
+        return jsonify({"message": "Error inesperado: " + str(e)}), 500
 
-# Update user
+
+# Actualizar un usuario
 @usuario_bp.route('/usuarios/<int:id_usuario>', methods=['PUT'])
 def update_usuario(id_usuario):
-    data = request.json
-    if not Usuario.validar_datos(data):
-        return jsonify({"error": "Invalid data"}), 400
-    Usuario.update(id_usuario, data)
-    return jsonify({"message": "User updated successfully"}), 200
+    try:
+        data = request.json
+        response = Usuario.update(id_usuario, data)  
+        return jsonify(response), 200
+    except DBError as e:
+        return jsonify({"message": str(e)}), 404
+    except Exception as e:
+        return jsonify({"message": "Error inesperado: " + str(e)}), 500
 
-# Delete user
+
+# Eliminar un usuario
 @usuario_bp.route('/usuarios/<int:id_usuario>', methods=['DELETE'])
 def delete_usuario(id_usuario):
-    Usuario.delete(id_usuario)
-    return jsonify({"message": "User deleted successfully"}), 200
+    try:
+        response = Usuario.delete(id_usuario)
+        return jsonify(response), 200
+    except DBError as e:
+        return jsonify({"message": str(e)}), 404
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
